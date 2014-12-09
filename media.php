a1f01c1557d8a70a62a67e25f53dfff33c96136c
Cody Romano <codyromano@gmail.com>
2014-12-08 23:36:51 -0500

    mend

--- media.php
+++ media.php
@@ -3,10 +3,13 @@
 require_once 'stdlib.php';
 
 if (isset($_GET['fileName'])) {
-	$filePath = DIR_MEMO_AUDIO . "/" . basename($_GET['fileName']); 
+	$basename = $_GET['fileName'];
+	$filePath = DIR_MEMO_AUDIO . "/" . $basename;
 
 	if (is_readable($filePath)) {
 		readfile($filePath); 
+	} else {
+		echo "Media '" . htmlentities(strip_tags($basename)) . "' not found";
 	}
 }
 
